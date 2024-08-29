7737fa8c9aae84d8044fab9d8fcbcd1bd84ae410
Guilherme Orozimbo <108307701+guiorozimbo@users.noreply.github.com>
2024-08-29 15:39:05 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')
+->prefix('/profile')
+->name('profile.')
+->controller(ProfileController::class)
+->group(function () {
+    Route::get('/',  'edit')->name('edit');
+    Route::patch('/', 'update')->name('update');
+    Route::delete('/', 'destroy')->name('destroy');
+});
+
+require __DIR__.'/auth.php';
